089fec9e4af28c16d8ea58ce6e1d7177c9548362
AdrianBaburac <132364709+AdrianBaburac@users.noreply.github.com>
2023-07-16 21:33:16 +0200

    Ivan je porpavio mapglitch
    OZO Z BOZO

--- Camera2D.gd
+++ Camera2D.gd
@@ -10,18 +10,18 @@ func _input(ev):
 	elif Input.is_action_just_pressed("scroll_up") and z < 15:
 		z+=1
 	elif Input.is_action_pressed("left_click"):
-		if ev is InputEventMouseMotion:
+		if ev is InputEventMouseMotion and abs(position.y)<map_size[1]*4 and abs(position.x)<map_size[0]*4:
 			position -= ev.relative / zoom
 	else:
 		return
 	
 	zoom = Vector2(z*0.01, z*0.01)
 	
-	if position.y >= map_size[1]*4:
-		position.y = -position.y +100
-	elif -position.y >= map_size[1]*4:
-		position.y = -position.y -100
-	elif position.x >= map_size[0]*4:
-		position.x = -position.x +100
-	elif -position.x >= map_size[0]*4:
-		position.x = -position.x -100
+	if position.y > map_size[1]*4:
+		position.y = -map_size[1] 
+	elif -position.y > map_size[1]*4:
+		position.y = map_size[1] 
+	elif position.x > map_size[0]*4:
+		position.x = -map_size[0] 
+	elif -position.x > map_size[0]*4:
+		position.x = map_size[0] 
